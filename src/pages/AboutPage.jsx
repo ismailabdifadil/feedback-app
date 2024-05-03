f2421a29485e1ead8c58cf698626cf9d460a42d2
Ismail <ismail.haabare@gmail.com>
2024-05-03 16:47:25 +0300

    about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,18 +1,19 @@
-import Card from "../components/shared/Card";
-import { Link } from "react-router-dom";
+import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
 const AboutPage = () => {
-  return <Card>
-    <div className="about">
-      <h1>About this project</h1>
-      <p>This is React App to leave a feedback for a product or services</p>
-      <p>Version: 1.0.0</p>
-      <p>
-        <Link to="/">Back To Home</Link>
-      
-      </p>
-    </div>
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this Project</h1>
+        <p>This is React App to leave a feedback for a product or services</p>
+        <p>Version: 1.0.0</p>
+        <p>
+          <Link to="/">Back To Home</Link>
+        </p>
+      </div>
       <p>Developed By Ismail Abdifadil Isack</p>
-  </Card>;
+    </Card>
+  );
 };
 
 export default AboutPage;
